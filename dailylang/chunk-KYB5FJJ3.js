091a9f25cd76ffd122da7e24d50ab64d1d771334
Ahmad <ahmad.moudallal1@hotmail.com>
2026-04-01 20:10:35 +0200

    update dailylang

--- dailylang/chunk-KYB5FJJ3.js
+++
@@ -1,1 +0,0 @@
-import{a,b,c}from"./chunk-L3M47J7B.js";import"./chunk-UNVCVEIH.js";import"./chunk-UUTQQLSI.js";export{a as extractYomitanText,c as parseJmdictSimplified,b as parseYomitanZip};
---
+++ dailylang/chunk-P6SAYF7R.js
@@ -0,0 +1,1 @@
+import{a,b,c}from"./chunk-U64VRTNV.js";import"./chunk-UNVCVEIH.js";import"./chunk-UUTQQLSI.js";export{a as extractYomitanText,c as parseJmdictSimplified,b as parseYomitanZip};
